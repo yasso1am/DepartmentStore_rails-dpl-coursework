8def31212a179cd36724a2dd656951e631d50bef
Andrew Yasso <andrew.yasso@gmail.com>
2018-05-30 16:03:17 -0600

    faker data populated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |dept|
+  dept = Department.create(
+    name: Faker::Commerce.department(1, true),
+  )
+
+20.times do
+  dept.items.create(
+    name: Faker::Commerce.product_name,
+    qty: Faker::Number.between(1, 20),
+    category: Faker::Commerce.material,
+    price: Faker::Number.between(2, 300),
+  )
+  end
+end
